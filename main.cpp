538740da5bccd83dc8cbf19b14ad75f13cc279c0
Dmitry <d@lyukov.com>
2017-09-29 02:54:33 +0300

    Implemented rotate on the 90x angles

--- main.cpp
+++ main.cpp
@@ -36,15 +36,62 @@ ImageBase<PixelType> Mirror( const ImageBase<PixelType> &image, const char axis 
     return result;
 }
 
+template <typename PixelType>
+ImageBase<PixelType> Rotate( const ImageBase<PixelType> &image,
+                             int angle = 90,
+                             bool direction = true )  // true = clockwise, false = counterclockwise
+{
+    if( direction == false )
+    {
+        angle = 360 - angle;
+    }
+    angle %= 360;
+    switch( angle )
+    {
+    case 0:
+        return image.Copy();
+    case 90:
+    {
+        ImageBase<PixelType> result = ImageBase<PixelType>( image.Height(), image.Width() );
+        for( int j = 0; j < image.Height(); ++j )
+        {
+            for( int i = 0; i < image.Width(); ++i )
+            {
+                result( image.Height() - j - 1, i ) = image( i, j );
+            }
+        }
+        return result;
+    }
+    case 180:
+    {
+        ImageBase<PixelType> result = ImageBase<PixelType>( image.Width(), image.Height() );
+        for( int j = 0; j < image.Height(); ++j )
+        {
+            for( int i = 0; i < image.Width(); ++i )
+            {
+                result( image.Width() - i - 1, image.Height() - j - 1 ) = image( i, j );
+            }
+        }
+        return result;
+    }
+    case 270:
+    {
+        ImageBase<PixelType> result = ImageBase<PixelType>( image.Height(), image.Width() );
+        for( int j = 0; j < image.Height(); ++j )
+        {
+            for( int i = 0; i < image.Width(); ++i )
+            {
+                result( j, image.Width() - i - 1 ) = image( i, j );
+            }
+        }
+        return result;
+    }
+    }
+    return image.Copy();
+}
+
 int main( int argc, char **argv )
 {
-    /* if( !strcmp( argv[1], "mirror" ) )
-     {
-     }*/
-    ColorByteImage image = ImageIO::FileToColorByteImage( argv[1] );
-    ColorByteImage image1 = Mirror( image, 'x' );
-    ColorByteImage image2 = Mirror( image, 'y' );
-    ImageIO::ImageToFile( image1, argv[2] );
-    ImageIO::ImageToFile( image2, argv[3] );
+    
     return 0;
 }
