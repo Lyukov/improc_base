07c113c2b64423891de71956545f09d7c23f9e8f
Dmitry <d@lyukov.com>
2017-09-29 02:03:25 +0300

    Implemented mirror reflection

--- main.cpp
+++ main.cpp
@@ -1,30 +1,49 @@
+#include <string.h>
 #include "imageformats.hpp"
 #include "imageio.hpp"
 
-float ToGray(ColorFloatPixel pixel)
+float ToGray( ColorFloatPixel pixel )
 {
-	return pixel.b * 0.114f + pixel.g * 0.587f + pixel.r * 0.299f;
+    return pixel.b * 0.114f + pixel.g * 0.587f + pixel.r * 0.299f;
 }
 
-void TestFunc(char *inputfilename, char *outputfilename)
+template <typename PixelType>
+ImageBase<PixelType> Mirror( const ImageBase<PixelType> &image, const char axis = 'x' )
 {
-	ColorFloatImage image = ImageIO::FileToColorFloatImage(inputfilename);
-	GrayscaleFloatImage res(image.Width(), image.Height());
-	
-	for (int j = 0; j < res.Height(); j++)
-		for (int i = 0; i < res.Width(); i++)
-			res(i, j) = ToGray(image(res.Width() - 1 - i, j));
-
-	ImageIO::ImageToFile(image, outputfilename);
+    assert( axis == 'x' || axis == 'y' );
+    ImageBase<PixelType> result = ImageBase<PixelType>( image.Width(), image.Height() );
+    if( axis == 'x' )
+    {
+        for( int j = 0; j < image.Height(); ++j )
+        {
+            for( int i = 0; i < image.Width(); ++i )
+            {
+                result( i, j ) = image( image.Width() - 1 - i, j );
+            }
+        }
+    }
+    else if( axis == 'y' )
+    {
+        for( int j = 0; j < image.Height(); ++j )
+        {
+            for( int i = 0; i < image.Width(); ++i )
+            {
+                result( i, j ) = image( i, image.Height() - 1 - j );
+            }
+        }
+    }
+    return result;
 }
 
-int main(int argc, char **argv)
+int main( int argc, char **argv )
 {
-	if (argc < 3)
-        return 0;
-    
-   // ColorFloatImage res = ImageIO::FileToColorFloatImage(argv[1]);
-   // ImageIO::ImageToFile(res, argv[2]);
-	TestFunc(argv[1], argv[2]);
-	return 0;
+    /* if( !strcmp( argv[1], "mirror" ) )
+     {
+     }*/
+    ColorByteImage image = ImageIO::FileToColorByteImage( argv[1] );
+    ColorByteImage image1 = Mirror( image, 'x' );
+    ColorByteImage image2 = Mirror( image, 'y' );
+    ImageIO::ImageToFile( image1, argv[2] );
+    ImageIO::ImageToFile( image2, argv[3] );
+    return 0;
 }
